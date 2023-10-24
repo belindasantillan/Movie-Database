52d70f7e828f2f1cd484344cf5406e1ded0f3dc8
Belinda Santillan <belindasarahisantillan@gmail.com>
2023-10-24 13:01:32 -0500

    JS Added

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 // Send all data requests http://www.omdbapi.com/?apikey=3846bb22&
 // Poster API requests http://img.omdbapi.com/?apikey=3846bb22&
 
+function onSearch(event) {
+    document.body.classList += ' home__loading'
+    const keyword = event.target.value;
+    localStorage.setItem("keyword", keyword);
+    window.location.href = `${window.location.origin}/movies.html`;
+}
---
+++ movies.js
@@ -0,0 +1,46 @@
+// API KEY 3846bb22
+// Omdb API http://omdbapi.com/?i=tt3896198&apikey=3846bb22
+// Send all data requests http://www.omdbapi.com/?apikey=3846bb22&
+// Poster API requests http://img.omdbapi.com/?apikey=3846bb22&
+
+
+const moviesListEl = document.querySelector(".movies__list");
+const keyword = localStorage.getItem("keyword");
+
+
+async function onSearchMovies(event) {
+    const keyword = event.target.value;
+    renderMovies(keyword);
+}
+
+async function renderMovies(keyword) {
+    const movies = await fetch(`http://www.omdbapi.com/?apikey=3846bb22&s=${keyword}`);
+    const moviesResponse = await movies.json();
+    const moviesData = moviesResponse.Search;
+
+    if (!moviesData) {
+        return document.querySelector('.results__banner--wrapper').style.display = "block"
+    }
+    else {
+        document.querySelector('.results__banner--wrapper').style.display = "none"
+        return moviesListEl.innerHTML = moviesData.map((movie) => movieHtml(movie)).slice(0,6).join('');
+    }
+    
+}
+
+
+function movieHtml(movie) {
+return `<div class="movie">
+<figure class="movie__img--wrapper">
+    <img src="${movie.Poster}" class="movie__img" alt="">
+</figure>
+<div class="movie__content--wrapper">
+    <h3 class="movie__title">${movie.Title}</h3>
+    <h6 class="movie__year">Year Released: ${movie.Year}</h6>
+</div>
+</div>`
+}
+
+
+renderMovies(keyword);
+
